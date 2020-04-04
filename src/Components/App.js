9e9cacd2f9feef20f8c67c9365527f58c1a93a6e
Xarizmo <mpolikarpoff@gmail.com>
2020-04-04 17:37:58 +0300

    #4 create files for each component

---
+++ src/Components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import TodoList from "./TodoList";
+import AppHeader from "./AppHeader";
+import SearchPanel from "./SearchPanel";
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader/>
+      <SearchPanel/>
+      <TodoList/>
+    </div>
+  );
+};
+
+export default App;
