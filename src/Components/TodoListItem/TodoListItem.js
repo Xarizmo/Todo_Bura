be0414814469b59a5994efda3eb67e732ea586f2
Xarizmo <mpolikarpoff@gmail.com>
2020-04-05 14:01:02 +0300

    #10 state & setState basic implementation

--- src/Components/TodoListItem/TodoListItem.js
+++ src/Components/TodoListItem/TodoListItem.js
@@ -7,38 +7,57 @@ export default class TodoListItem extends Component {
   constructor() {
     super();
     
+    this.state = {
+      done: false,
+      important: false
+    };
+    
     this.onLabelClick = () => {
-      console.log(`Done: ${this.props.label}`);
+      this.setState({
+        done: true,
+      });
+    };
+    
+    this.onExclamationButtonClick = () => {
+      this.setState({
+        important: true
+      });
     };
   }
   
   render() {
+    const { label } = this.props;
+    const { done, important } = this.state;
     
-    const {label, important = false} = this.props;
+    let classNames = "todo-list-item";
     
-    const style = {
-      color: important ? 'steelblue' : 'black',
-      fontWeight: important ? 'bold' : 'normal'
-    };
+    if (done) {
+      classNames += ' done';
+    }
+    
+    if (important) {
+      classNames += ' important';
+    }
     
     return (
-      <span className="todo-list-item">
+      <span className={classNames}>
         <span
-          className="todo-list-item-label"
-          style={style}
+          className='todo-list-item-label'
           onClick={ this.onLabelClick } >
           {label}
         </span>
   
         <button type="button"
-                className="btn btn-outline-success btn-sm float-right">
-          <i className="fa fa-exclamation"/>
-        </button>
-  
-        <button type="button"
                 className="btn btn-outline-danger btn-sm float-right">
           <i className="fa fa-trash-o"/>
         </button>
+        
+        <button type="button"
+                className="btn btn-outline-success btn-sm float-right"
+                onClick={ this.onExclamationButtonClick } >
+          <i className="fa fa-exclamation"/>
+        </button>
+  
       </span>
     );
   }
