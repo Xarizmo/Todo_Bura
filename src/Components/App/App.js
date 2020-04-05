c119f0f62514eb1b29361ee7f36480b836b7d184
Xarizmo <mpolikarpoff@gmail.com>
2020-04-06 02:42:32 +0300

    #15 implementing onToggleDone & onToggleImportant

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,26 +7,25 @@ import './App.css'
 import ItemAddForm from "../ItemAddForm/ItemAddForm";
 
 export default class App extends Component {
-  constructor() {
-    super();
-    
-    this.state = {
-      todoData: [
-        {label: 'Drink Coffee', important: false, id: 1},
-        {label: 'Make Awesome App', important: true, id: 2},
-        {label: 'Have a lunch', important: false, id: 3}
-      ]
-    };
-    
-    this.arrLength = this.state.todoData.length + 1;
-  }
+
+  maxId = 100;
+  
+  state = {
+    todoData: [
+      this.createTodoItem('Drink Coffee'),
+      this.createTodoItem('Make Awesome App'),
+      this.createTodoItem('Have a lunch')
+    ]
+  };
   
   deleteItem = (id) => {
     this.setState(({todoData}) => {
       const idx = todoData.findIndex((el) => el.id === id);
-      const before = todoData.slice(0, idx);
-      const after = todoData.slice(idx + 1);
-      const newArray = [...before, ...after];
+      
+      const newArray = [
+        ...todoData.slice(0, idx),
+        ...todoData.slice(idx + 1)
+      ];
       
       return {
         todoData: newArray
@@ -34,12 +33,19 @@ export default class App extends Component {
     });
   };
   
+  createTodoItem(label) {
+    return {
+      label,
+      important: false,
+      done: false,
+      id: this.maxId++
+    }
+  }
+  
   addItem = (text) => {
-    // формируем новый item - label пока будем получать как передаваемый текст, id генерим отталкиваясь от текущей длины массива с items
-    const newItem = {label: text, important: false, id: this.arrLength++};
+    const newItem = this.createTodoItem(text);
     
-    // формируем новый массив на основе текущего todoData и новый item, который мы сгенерили
-    this.setState(({ todoData }) => {
+    this.setState(({todoData}) => {
       const newArr = [
         ...todoData,
         newItem
@@ -51,27 +57,54 @@ export default class App extends Component {
     });
   };
   
-  onToggleImportant = (id) => {
-    console.log('Toggle Important', id);
-  };
+  toggleProperty(arr, id, propName) {
+    const idx = arr.findIndex((el) => el.id === id);
+    
+    const oldItem = arr[idx];
+    const newItem = {...oldItem, [propName]: !oldItem[propName]};
+    
+    return [
+      ...arr.slice(0, idx),
+      newItem,
+      ...arr.slice(idx + 1)
+    ];
+  }
   
   onToggleDone = (id) => {
-    console.log('Toggle Done', id);
+    this.setState(({ todoData }) => {
+      return {
+        todoData: this.toggleProperty(todoData, id, 'done')
+      };
+    });
   };
   
+  onToggleImportant = (id) => {
+    this.setState(({ todoData }) => {
+      return {
+        todoData: this.toggleProperty(todoData, id, 'important')
+      };
+    });
+  };
+
+  
   render() {
+    
+    const { todoData } = this.state;
+    const doneCount = todoData.filter((el) => el.done).length;
+    const todoCount = todoData.length - doneCount;
+    
     return (
       <div className='todo-app'>
-        <AppHeader toDo={1} done={3}/>
+        <AppHeader toDo={todoCount} done={doneCount}/>
         <div className='top-panel d-flex'>
           <SearchPanel/>
           <ItemStatusFilter/>
         </div>
         <TodoList
-          todos={this.state.todoData}
+          todos={todoData}
           onDeleted={this.deleteItem}
           onToggleImportant={this.onToggleImportant}
-          onToggleDone={this.onToggleDone} />
+          onToggleDone={this.onToggleDone}/>
         <ItemAddForm
           onAddItem={this.addItem}/>
       </div>
