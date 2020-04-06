6cae719d8a5f8e30953d0b7b769c1b282fbdffd0
Xarizmo <mpolikarpoff@gmail.com>
2020-04-06 12:16:30 +0300

    #16 add custom name TodoItem functionality

--- src/Components/ItemAddForm/ItemAddForm.js
+++ src/Components/ItemAddForm/ItemAddForm.js
@@ -2,17 +2,35 @@ import React, {Component} from "react";
 import './ItemAddForm.css';
 
 export default class ItemAddForm extends Component {
+  
+  state = {
+    label: ''
+  };
+  
+  onLabelChange = (e) => {
+    this.setState({
+      label: e.target.value
+    });
+  };
+  
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onAddItem(this.state.label);
+  };
+  
   render() {
-    const { onAddItem } = this.props;
-    
     return (
-      <div className='item-add-form'>
+      <form className='item-add-form d-flex'
+            onSubmit={this.onSubmit}>
+        <input type="text"
+               className='form-control'
+               onChange={this.onLabelChange}
+               placeholder='What needs to be done' />
         <button
-          className='btn btn-outline-secondary'
-          onClick={() => onAddItem('Hello World')}>
+          className='btn btn-outline-secondary'>
           Add Item
         </button>
-      </div>
+      </form>
     )
   }
 };
